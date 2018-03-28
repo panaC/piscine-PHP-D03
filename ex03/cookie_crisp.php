77345d5dd906f925a5cb636255d3ff9948d7afff
pleroux <p.leroux@gmx.com>
2018-03-28 19:27:25 +0200

    mod

--- ex03/cookie_crisp.php
+++ ex03/cookie_crisp.php
@@ -7,7 +7,7 @@
  */
 
 if (isset($_COOKIE['array'])) {
-    $arr = unserialize($_COOKIE['array']);
+    $arr = $_COOKIE['array'];
 } else
     $arr = array();
 
@@ -15,14 +15,14 @@ if (isset($_GET['action']) && isset($_GET['name']))
 {
     if ($_GET['action'] === "set" && isset($_GET['value'])){
         $arr[trim($_GET['name'])] = trim($_GET['value']);
-        setcookie("array", serialize($arr));
+        setcookie("array", $arr[trim($_GET['name'])]);
 
     } else if ($_GET['action'] === "get") {
         if (isset($arr[trim($_GET['name'])])) {
             echo $arr[trim($_GET['name'])] . "\n";
         }
     } else if ($_GET['action'] === "del"){
-        setcookie("array", serialize($arr));
+        setcookie("array", "");
     }
 }
 
